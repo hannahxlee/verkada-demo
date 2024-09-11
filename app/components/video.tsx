7b9e1ff7d23bff2f71e84dcf9e2dfacbd27e16d8
Hannah Lee <hannah.lee@Hannah-Lee-L0WK6V4PXK.local>
2024-09-11 13:53:11 -0700

    add more gifs

--- app/components/video.tsx
+++ app/components/video.tsx
@@ -1,8 +1,7 @@
-'use client';
+"use client";
 import { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 import Image from "next/image";
-
 
 export default function Video() {
   const ref = useRef(null);
@@ -22,17 +21,42 @@ export default function Video() {
         />
       </svg>
       <ul ref={ref} className="mt-32">
-        <li><Image src="/SENSOR_ANALYTICS.gif" alt="ANALYTICS" width={300} height={651} className="rounded-xl"/></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <Image
+            src="/SENSOR_ANALYTICS.gif"
+            alt="HOME"
+            width={300}
+            height={651}
+            className="rounded-xl"
+          />
+        </li>
+        <li>
+          <Image
+            src="/SENSOR_DASHBOARDS.gif"
+            alt="DASHBOARDS"
+            width={300}
+            height={651}
+            className="rounded-xl"
+          />
+        </li>
+        <li>
+          <Image
+            src="/SENSOR_ANALYTICS.gif"
+            alt="ANALYTICS"
+            width={300}
+            height={651}
+            className="rounded-xl"
+          />
+        </li>
+        <li>
+          <Image
+            src="/SENSOR_CALIBRATION.gif"
+            alt="CALIBRATION"
+            width={300}
+            height={651}
+            className="rounded-xl"
+          />
+        </li>
       </ul>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,17 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center py-24 overflow-hidden">
       <div className="text-center text-navy">
-        <div className={`${poppins.variable} font-sans font-bold text-xl`}>Sensor WebFusionX</div>
-        <div className={`${poppins.variable} font-sans`}>BY HANNAH LEE</div>
+        <div className={`${poppins.variable} font-sans font-bold text-xl`}>
+          Sensor WebFusionX
+        </div>
+        <div className={`${poppins.variable} font-sans text-sm`}>
+          home &#183; dashboards &#183; analytics &#183; calibrations{" "}
+        </div>
+        <div
+          className={`${poppins.variable} font-sans absolute bottom-2 right-4 text-sm`}
+        >
+          BY HANNAH LEE
+        </div>
       </div>
       <Video />
     </div>
