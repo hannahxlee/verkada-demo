c67a597f5ecef9f9d515b9e62bb01ea95510b4b9
Hannah Lee <hannah.lee@Hannah-Lee-L0WK6V4PXK.local>
2024-09-13 11:12:30 -0700

    add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center py-24 md:py-28 overflow-hidden">
       <div className="text-center text-navy md:space-y-2">
-        <div className={`${poppins.variable} font-sans font-bold text-xl md:text-3xl`}>
+        <div
+          className={`${poppins.variable} font-sans font-bold text-xl md:text-3xl`}
+        >
           Sensor WebFusionX
         </div>
         <div className={`${poppins.variable} font-sans text-sm md:text-lg`}>
